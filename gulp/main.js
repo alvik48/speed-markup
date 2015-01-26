d972c91e445d5b6090baf708586986736c380902
alvik48 <alvik48@gmail.com>
2015-01-26 15:43:27 +0600

    * added 'images' task for copy images to path

--- gulp/main.js
+++ gulp/main.js
@@ -19,10 +19,11 @@ tasks.forEach(function(task) {
     Default task
 ============================================== */
 
-gulp.task('default', ['server', 'browserSync', 'sprite', 'base64', 'styles', 'js', 'html'], function() {
+gulp.task('default', ['server', 'browserSync', 'sprite', 'base64', 'styles', 'js', 'images', 'html'], function () {
     gulp.watch('src/styles/**/*.styl', ['styles']);
-    gulp.watch('src/images/sprite/*.png', ['sprite']);
-    gulp.watch('src/images/inline/*.png', ['base64']);
+    gulp.watch('src/images/sprite/*.*', ['sprite']);
+    gulp.watch('src/images/inline/*.*', ['base64']);
+    gulp.watch(['src/images/**/*.*', '!src/images/sprite/*.*', '!src/images/inline/*.*'], ['images']);
     gulp.watch(['src/js/**/*.js', '!public/js/app.js'], ['js']);
     gulp.watch('src/html/**/*.html', ['html']);
 });
