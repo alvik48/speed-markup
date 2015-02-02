f3cdf05bfb0580dae09e57e16be78d2e88189979
alvik48 <alvik48@gmail.com>
2015-02-02 12:10:08 +0600

    - uninstalled browserify-shim * refactored js

--- src/js/models/link.model.js
+++ src/js/models/link.model.js
@@ -1,5 +1,10 @@
 'use strict';
 
+var _ = require('../../bower_components/underscore/underscore');
+var $ = require('../../bower_components/jquery/dist/jquery');
+var Backbone = require('../../bower_components/backbone/backbone');
+Backbone.$ = $;
+
 /* ==============================================
     Model
 ============================================== */
